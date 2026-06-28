af7537a536fa4758a718408c626a3b0f4dfc304b
Nhat Nguyen <nghminhnhat2006@gmail.com>
2026-06-28 17:24:46 +0700

    Init project: Vite + React 19 + Bootstrap 5

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import React from 'react'
+import Orchids from './components/Orchids'
+import './App.css'
+
+function App() {
+    return <Orchids />
+}
+
+export default App
